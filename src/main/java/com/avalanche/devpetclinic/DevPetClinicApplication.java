19fc98108cd80c616a6a2383b257c6c7e092e7d5
Joan <joan.almeida@redb.ee>
2020-05-08 23:58:35 -0300

    Initial project files

---
+++ src/main/java/com/avalanche/devpetclinic/DevPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.avalanche.devpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avalanche/devpetclinic/DevPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalanche.devpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
